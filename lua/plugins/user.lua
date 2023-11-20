19ae6459e6c686f158545d8405c8ef9fc7929ab1
NguyenHoangHieu22GIT2 <133379564+NguyenHoangHieu22GIT2@users.noreply.github.com>
2023-11-20 13:16:24 +0700

    Update

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -11,15 +11,74 @@
 --
 return {
   -- add gruvbox
+  {
+    "nvimdev/dashboard-nvim",
+    event = "VimEnter",
+    opts = function(_, opts)
+      local logo = [[
+██╗  ██╗██╗  ██╗██╗███████╗██╗   ██╗███████╗
+██║  ██║██║  ██║██║██╔════╝██║   ██║╚══███╔╝
+███████║███████║██║█████╗  ██║   ██║  ███╔╝ 
+██╔══██║██╔══██║██║██╔══╝  ██║   ██║ ███╔╝  
+██║  ██║██║  ██║██║███████╗╚██████╔╝███████╗
+╚═╝  ╚═╝╚═╝  ╚═╝╚═╝╚══════╝ ╚═════╝ ╚══════╝
+                                            
+      ]]
+
+      logo = string.rep("\n", 8) .. logo .. "\n\n"
+      opts.config.header = vim.split(logo, "\n")
+    end,
+  },
+  {
+  "b0o/incline.nvim",
+  dependencies = { "craftzdog/solarized-osaka.nvim" },
+  event = "BufReadPre",
+  priority = 1200,
+  config = function()
+    local colors = require("solarized-osaka.colors").setup()
+    require("incline").setup({
+      highlight = {
+        groups = {
+          InclineNormal = { guibg = colors.magenta500, guifg = colors.base04 },
+          InclineNormalNC = { guifg = colors.violet500, guibg = colors.base03 },
+        },
+      },
+      window = { margin = { vertical = 0, horizontal = 1 } },
+      hide = {
+        cursorline = true,
+      },
+      render = function(props)
+        local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
+        if vim.bo[props.buf].modified then
+          filename = "[+] " .. filename
+        end
+
+        local icon, color = require("nvim-web-devicons").get_icon_color(filename)
+        return { { icon, guifg = color }, { " " }, { filename } }
+      end,
+    })
+  end,
+},
   { "ellisonleao/gruvbox.nvim" },
   -- -- Configure LazyVim to load gruvbox
    {"mg979/vim-visual-multi"},
-  {"craftzdog/solarized-osaka.nvim"},
+  {
+    "folke/noice.nvim",
+    opts = function(_, opts)
+      opts.presets.lsp_doc_border = true
+      table.insert(opts.routes, {
+				filter = {
+					event = "notify",
+					find = "No information available",
+				},
+				opts = { skip = true },
+      })
+    end,
+  },
   {
 
     "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "solarized-osaka",
+    opts = { colorscheme = "solarized-osaka",
     },
   },
   --
@@ -179,9 +238,13 @@ keys = { { "<leader>cs", "<cmd>SymbolsOutline<cr>", desc = "Symbols Outline" } }
   {
     "nvim-lualine/lualine.nvim",
     event = "VeryLazy",
-    opts = function(_, opts)
-      table.insert(opts.sections.lualine_x, "😄")
-    end,
+      opts = { options = {
+          theme = "solarized_dark"
+        }
+      }
+    -- opts = function(_, opts)
+    --   table.insert(opts.sections.lualine_x, "😄")
+    -- end,
   },
 
   -- or you can return new options to override all the defaults
